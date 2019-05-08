083c777dae71ca090b7637d7836dbb26c9ac3f45
futuregit <gmail.com>
2019-05-08 17:19:58 -0400

    Changed to pure components to smart components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import  classes from './Person.css';
 
-const person = (props) => {
-  console.log("Inside Person.js render")
-  // const rnd = Math.random();
-  //
-  // if (rnd > 0.7) {
-  //   throw new Error('Something went wrong');
-  // }
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>This is the person module and the name chosen is {props.name}</p>
-      <p>Wow that was easy {props.age}</p>
-      <p>{props.children}</p>
-      <input type="test" onChange={props.changed} value={props.name} />
-    </div>
-  )
+class Person extends Component {
+  render(){
+    console.log("Inside Person.js render")
+    // const rnd = Math.random();
+    //
+    // if (rnd > 0.7) {
+    //   throw new Error('Something went wrong');
+    // }
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>This is the person module and the name chosen is {this.props.name}</p>
+        <p>Wow that was easy {this.props.age}</p>
+        <p>{this.props.children}</p>
+        <input type="test" onChange={this.props.changed} value={this.props.name} />
+      </div>
+    )
+  }
+
 };
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ErrorBoundary from '../../containers/ErrorBoundary/ErrorBoundary';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-  console.log("Inside Persons.js render");
-    //Watch putting JSX <ErrorBoundary> on next line. It causes errors.
-    //Possibly related to JavaScript ASI(Automatic Semicolon Insertion)
-    return <ErrorBoundary key={person.id}>
-        <Person
-          click={() => props.clicked(index)}
-          name={person.name}
-          age={person.age}
-          //Get the event object and pass in the person.id of this item
-          changed={(event) => props.changed(event, person.id)}/>
-      </ErrorBoundary>
-  });
+class Persons extends Component {
+  render() {
+    return this.props.persons.map((person, index) => {
+    console.log("Inside Persons.js render");
+      //Watch putting JSX <ErrorBoundary> on next line. It causes errors.
+      //Possibly related to JavaScript ASI(Automatic Semicolon Insertion)
+      return <ErrorBoundary key={person.id}>
+          <Person
+            click={() => this.props.clicked(index)}
+            name={person.name}
+            age={person.age}
+            //Get the event object and pass in the person.id of this item
+            changed={(event) => this.props.changed(event, person.id)}/>
+        </ErrorBoundary>
+    });
+  }
+}
 
-export default persons;
+export default Persons;
